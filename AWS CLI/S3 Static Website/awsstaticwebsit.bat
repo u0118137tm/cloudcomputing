1fc3dde9a26db53a9a8be0810bfbe43224a571e7
u0118137tm <u0118137@thomasmore.be>
2024-10-25 10:05:38 +0200

    s3 static website

--- AWS CLI/S3 Static Website/awsstaticwebsit.bat
+++
@@ -1,7 +0,0 @@
-SET /P bucketname= Please enter bucket name:
-CALL aws s3api create-bucket --bucket %bucketname% --region eu-west-1 --create-bucket-configuration LocationConstraint=eu-west-1
-CALL aws s3 website s3://%bucketname%/ --index-document index.html --error-document error.html
-CALL aws s3 cp index.html s3://%bucketname% --acl public-read
-CALL aws s3 cp error.html s3://%bucketname% --acl public-read
-CALL echo website url: http://%bucketname%.s3-website-eu-west-1.amazonaws.com
-pause
